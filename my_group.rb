c119fe3f7d8e14c1de5bf26a7834e371e1a54e45
nader <alghazu@gmail.com>
2018-03-13 20:53:59 +0400

    First ruby numbers

---
+++ my_group.rb
@@ -0,0 +1,5 @@
+person_1 = {name:"Andy", gender:"M", age: 34}
+person_2 = {name:"Rami", gender:"M", age: 42}
+person_3 = {name:"Yara", gender:"F", age: 33}
+
+my_group = [person_1,person_2,person_3]
