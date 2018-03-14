3d8673e5d8b61cecf6d99226f36d2bfd1dab36b7
nader <alghazu@gmail.com>
2018-03-14 18:21:15 +0400

    end of pine.fm excersises

--- calc.rb
+++ calc.rb
@@ -5,3 +5,10 @@ middle = gets.chomp
 puts 'What is your family name?'
 family = gets.chomp
 puts 'Nice to meet you ' + first + ' ' + middle +' ' + family
+
+puts '
+
+What is your favourite number?'
+favNum = gets.to_i
+favNum = favNum +1
+puts 'Why not try ' + favNum.to_s + ' it is bigger and better!'
